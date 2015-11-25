86dd980b28262e3ddf665f300d80ecfbfce29f4d
dac482 <diegocarrillo482@gmail.com>
2015-11-24 20:20:20 -0600

    algunos cambios en el inicio

---
+++ contactanos.php
@@ -0,0 +1,172 @@
+
+<!DOCTYPE HTML>
+<!--
+	Helios by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Contáctanos - AudiTec</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+
+<link type="text/css" rel="stylesheet" href="main.css">
+
+
+
+
+
+	</head>
+	<body class="no-sidebar">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header">
+
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+								<h1><a href="index.html" id="logo">ConsulTIC</a></h1>
+							</header>
+						</div>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+							<li><a href="index.html">Inicio</a></li>
+								<li><a href="portafolio.html">Portafolio</a></li>
+								<li><a href="quienes.html">Quiénes somos</a></li>
+								<li><a href="contactanos.php">Contáctanos</a></li>
+							</ul>
+						</nav>
+
+				</div>
+
+			<!-- Main -->
+				<div class="wrapper style1">
+
+					<div class="container">
+						<article id="main" class="special">
+							<header>
+								<h2><a href="#">Contáctanos</a></h2>
+								<p>
+									Déjanos tu opinión, queja, sugerencia o comentario.
+									<BR>
+
+									Los datos proporcionados serán tratados con total confidencialidad.
+								</p>
+								
+									
+								
+							</header>
+							
+							
+								</header>
+								<p>
+
+								<title>Formulario de contacto</title>
+								</head>
+								<body>
+
+
+									<!-- Formulario -->
+								<section class="formulario">
+								<form action="contac.php"  method="post">
+									 <label for="nombre">Nombre:</label>
+									 <input id="nombre" type="text" name="nombre" placeholder="Nombre" required />
+
+									 <label for="email">Email:</label>
+									 <input id="email" type="email" name="email" placeholder="ejemplo@correo.com" required/>
+
+									 <label for="mensaje">Mensaje:</label>
+									 <textarea id="mensaje" name="mensaje" placeholder="Mensaje" required/></textarea>
+
+									 &nbsp;&nbsp;<input id="submit" type="submit" name="submit" value="Enviar" />
+
+                                </form>
+							    </section>
+								<header ><a href="Aviso/aviso.pdf" target="_blank">Aviso de privacidad</a></header>
+									<!-- Fin del formulario -->
+									
+							    </p>
+								
+								
+							</section>
+							
+
+
+
+
+						</article>
+						
+					
+					</div>
+
+				</div>
+
+			<!-- Footer -->
+				<div id="footer">
+					<div class="container">
+						<div class="row">
+
+					
+						
+
+							
+
+						</div>
+						<hr />
+						
+						<div class="row">
+							<div class="12u">
+
+								<!-- Contact -->
+									<section class="contact">
+										<header>
+											<h3>Nuestra ubicación</h3>
+										</header>
+										<p>Carretera Durango - Mezquital K.M 4.5.. Gabino Santillán, C.P. 34308.
+											<br>
+												También puedes contáctarnos a través de nuestras redes sociales, correo eléctronico consultic9@gmail.com o al teléfono (+52) 618-159-34-96.
+
+
+										</p>
+										<ul class="icons">
+											<li><a href="https://twitter.com/ConsulTIC_UTD" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+											<li><a href="https://www.facebook.com/pages/Consultic/1683349825231228?fref=ts" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+								
+										</ul>
+									</section>
+
+
+								<!-- Copyright -->
+									<div class="copyright">
+										<ul class="menu">
+													<li>&copy; Todos los derechos reservados.</li><li>Consul<a href="index.html">TIC</a></li><li><a href="login.php">Administrador</a></li>
+										</ul>
+									</div>
+
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.onvisible.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
